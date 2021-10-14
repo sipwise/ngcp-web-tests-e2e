2c433509300e4dc8c07e54e930957d7b1bc9df7d
Sergii Leonenko <sleonenko@sipwise.com>
2021-10-14 16:58:13 +0300

    TT#128155 TestRunner - Create Docker environment in ngcp-web-test-e2e to be able to run Cypress tests within a common environment
    AC:
    Can run Cypress tests (cmd => "yarn ci:run:csc-ui") within a Docker environment based on Docker file from ngcp-admin-ui
    Can run Docker including Cypress tests from a bash script
    
    Change-Id: I302e990742a46126db6c246a340dc71eafcec038

--- README.md
+++ README.md
@@ -1,6 +1,34 @@
 # End-to-End tests for the WebUIs in the Sipwise VoIP Platform
 
-## How to run
+
+### Special information related to each individual tests subproject
 
 * [ngcp-csc-ui (Customer Self-Care WebUI)](docs/ngcp-csc-ui.md)
 * [ngcp-admin-ui (Administration WebUI)](docs/ngcp-admin-ui.md)
+## How to run
+### Running test in Docker
+To run tests in already prepared docker image you just need to execute next command:
+```bash
+yarn run docker:run <parameters for the tests>
+```
+For example our parameters are "< CSC UI URL >  < CSC API URL >"
+
+```bash
+yarn run docker:run https://dev-web-trunk.mgm.sipwise.com/v2 https://dev-web-trunk.mgm.sipwise.com
+```
+
+In case you would like have you own local Docker image to apply custom version of Cypress you need to build a local Docker image.
+For that please follow next steps:
+1. Prepare desired version of "package.json"
+2. Build your local Docker image with `yarn run docker:rebuild:local`
+3. Run tests in Docker container with `yarn run docker:run:local <tests parameters>`
+
+For example:
+```bash
+yarn run docker:rebuild:local
+yarn run docker:run:local https://host.docker.internal:8080 https://dev-web-trunk.mgm.sipwise.com
+```
+
+Note: if you would like to test CSC or AUI applications started locally you should pass `https://host.docker.internal:8080` instead of `https://localhost:8080` to the tests running script!
+
+Important: if you test locally started application (CSC UI URL: `https://host.docker.internal:8080` or `https://localhost:8080`) you should change CSC user logins in your test's config to be with domain name, like so: `username@domain`. Otherwise, some tests might fail.
---
+++ bin/run-docker.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -e
+
+readonly DOCKER_GLOBAL_IMAGE_NAME='docker.mgm.sipwise.com/ngcp-web-tests-e2e-bullseye:latest'
+readonly DOCKER_LOCAL_IMAGE_NAME='ngcp-web-tests-e2e-bullseye:latest'
+
+echo "Running Cypress test headless in Docker"
+
+if [ $# -eq 0 ] || { [ $# -eq 1 ] && [ "$1" = "local" ]; }; then
+  echo "Error: configuration parameters are missing. " >&2
+  echo "    Parameters format: '[local] <CSC UI URL> <CSC API URL>' " >&2
+  echo "    Example: 'local https://host.docker.internal:8080 https://dev-web-trunk.mgm.sipwise.com'" >&2
+  exit 1
+fi
+
+if [ "$1" = "local" ]; then
+  shift
+  DOCKER_IMAGE_NAME="${DOCKER_LOCAL_IMAGE_NAME}"
+  echo "Using local docker image: ${DOCKER_IMAGE_NAME}"
+else
+  DOCKER_IMAGE_NAME="${DOCKER_GLOBAL_IMAGE_NAME}"
+  echo "Pulling docker image: ${DOCKER_IMAGE_NAME}"
+  docker pull "${DOCKER_IMAGE_NAME}"
+fi
+
+export MSYS_NO_PATHCONV=1    # might be used to solve automatic path transformation in Git Bash console on Windows platform
+                              # try to use "/$PWD..." instead of "$(pwd)..." with Git Bash on Win
+
+ARGS=( "$@" )
+
+RESULTS_FOLDER="${PWD}/docker_results"
+mkdir -p "${RESULTS_FOLDER}"
+touch "${RESULTS_FOLDER}/console.log"
+
+docker run --rm -i -t \
+  -v "/${PWD}/cypress:/code/cypress:ro" \
+  -v "/${PWD}/package.json:/code/package.json:ro" \
+  -v "/${PWD}/cypress.template.json:/code/cypress.template.json:ro" \
+  -v "/${PWD}/t/run_web_e2e:/code/run_web_e2e:ro" \
+  -v "/${RESULTS_FOLDER}/screenshots:/code/cypress/screenshots:rw" \
+  -v "/${RESULTS_FOLDER}/videos:/code/cypress/videos:rw" \
+  --entrypoint "/bin/bash" \
+  "${DOCKER_IMAGE_NAME}" ./run_web_e2e "${ARGS[@]}" \
+       2>&1 | tee "${RESULTS_FOLDER}/console.log"
+
+# Note: to read "console.log" file you could use "less -R docker_results/console.log"  command
+
+exit "${PIPESTATUS[0]}"
