df7dc014222eb424a1153d2aff527251421bbc9b
Nico Schedel <nschedel@sipwise.com>
2021-12-20 13:22:03 +0100

    TT#155404 More improvements for domain tests
    - Check if empty values get rejected
    - Edit a preference (allowed_ips) in domain
    - Delete Domain
    
    Change-Id: Ia6854dfc6d88cd26168e462860ab0ddc5093fec3

--- cypress/integration/ngcp-admin-ui/domains.spec.js
+++ cypress/integration/ngcp-admin-ui/domains.spec.js
@@ -1,39 +1,91 @@
+/// <reference types="cypress" />
+
+import {
+    getRandomNum,
+    waitPageProgress,
+    deleteItemOnListPageByName,
+    clickDataTableSelectedMoreMenuItem
+} from '../../support/ngcp-admin-ui/utils/common'
 
 const ngcpConfig = Cypress.config('ngcpConfig')
-const authConfig = {
-    username: ngcpConfig.username,
-    password: ngcpConfig.password
-}
+const domainName = 'domain' + getRandomNum()
 
-context('Domains', () => {
-    it('should create a new Domain', () => {
-        cy.login(ngcpConfig.username, ngcpConfig.password)
-        cy.request({
-            method: 'GET',
-            url: ngcpConfig.apiHost + '/api/resellers/1',
-            auth: authConfig
-        }).its('body').as('defaultReseller').then(function () {
-            const randomDomainName = 'domain' + Date.now()
+context('Domain tests', () => {
+    context('UI domain tests', () => {
+        before(() => {
+            Cypress.log({ displayName: 'API URL', message: ngcpConfig.apiHost })
+        })
+
+        it('Check if domain with invalid values gets rejected', () => {
+            cy.login(ngcpConfig.username, ngcpConfig.password)
             cy.navigateMainMenu('settings / domain-list')
             cy.locationShouldBe('#/domain')
             cy.get('[data-cy=aui-list-action--domain-creation]').click()
-            cy.intercept('/reseller/ajax*').as('filterResellers')
-            cy.get('[data-cy=aui-select-reseller] input').type(this.defaultReseller.name)
-            cy.wait('@filterResellers').its('response.statusCode').should('eq', 200)
-            cy.wait('@filterResellers').its('response.statusCode').should('eq', 200)
-            cy.get('[data-cy=aui-select-reseller]').then($el => {
-                cy.get(`#${$el.attr('for')}_lb .q-item`).eq(0).click()
-            })
-            cy.get('[data-cy=aui-new-domain] .q-item:eq(1) input').type(randomDomainName)
-            cy.intercept('/api/domains', {
-                statusCode: 200,
-                body: {
-                    name: randomDomainName
-                }
-            }).as('domainCreation')
             cy.get('[data-cy=aui-save-button]').click()
-            cy.wait('@domainCreation')
+            cy.contains('[data-cy="aui-domain-creation"] div[role=alert]', 'Input is required').should('be.visible')
+        })
+
+        it('Create a domain', () => {
+            cy.login(ngcpConfig.username, ngcpConfig.password)
+            cy.navigateMainMenu('settings / domain-list')
             cy.locationShouldBe('#/domain')
+            cy.get('[data-cy=aui-list-action--domain-creation]').click()
+            cy.auiSelectLazySelect({ dataCy: 'aui-select-reseller', filter: 'default', itemContains: 'default' })
+            cy.get('[data-cy=aui-new-domain] .q-item:eq(1) input').type(domainName)
+            cy.get('[data-cy=aui-save-button]').click()
+            cy.contains('.q-notification', 'Domain created successfully').should('be.visible')
+        })
+
+        it('Add/Reset/Delete a preference (allowed_ips) in domain', () => {
+            cy.login(ngcpConfig.username, ngcpConfig.password)
+            cy.navigateMainMenu('settings / domain-list')
+
+            cy.locationShouldBe('#/domain')
+
+            cy.get('label[data-cy="aui-input-search"]').type(domainName)
+            waitPageProgress()
+            cy.get('[data-cy=aui-data-table] .q-checkbox').click()
+            clickDataTableSelectedMoreMenuItem('domain-preferences')
+
+            cy.get('[data-cy="q-item--allowed-ips"]').should('be.visible').as('aIPSetting')
+
+            const ipButtons = {
+                ipv4button: '[data-cy="q-chip--10-0-0-1',
+                ipv6button: '[data-cy="q-chip--2001-db-8-3333-4444-cccc-dddd-eeee-ffff',
+                ipv4: '10.0.0.1',
+                ipv6: '2001:db8:3333:4444:CCCC:DDDD:EEEE:FFFF'
+            }
+
+            cy.get('@aIPSetting').find('input').type('invalid')
+            cy.get('@aIPSetting').contains('button[data-cy="q-btn"]', 'Add').click()
+            cy.get('@aIPSetting').contains('div[role=alert]', 'Input must be a valid IPv4 or IPv6').should('be.visible')
+            cy.get('@aIPSetting').find('input').clear().type(ipButtons.ipv4)
+            cy.get('@aIPSetting').contains('button[data-cy="q-btn"]', 'Add').click()
+            waitPageProgress()
+            cy.get('@aIPSetting').find(ipButtons.ipv4button + '0"]').should('contain.text', ipButtons.ipv4)
+            cy.get('@aIPSetting').find('input').type(ipButtons.ipv6)
+            cy.get('@aIPSetting').contains('button[data-cy="q-btn"]', 'Add').click()
+            waitPageProgress()
+            cy.get('@aIPSetting').find(ipButtons.ipv6button + '-1"]').should('contain.text', ipButtons.ipv6)
+            cy.get('@aIPSetting').find(ipButtons.ipv4button + '0"] .q-chip__icon--remove').click()
+            waitPageProgress()
+            cy.get('@aIPSetting').find(ipButtons.ipv6button + '-0"]').should('be.visible')
+            cy.get('@aIPSetting').find(ipButtons.ipv4button + '0"]').should('not.exist')
+            cy.get('@aIPSetting').find('input').type(ipButtons.ipv4)
+            cy.get('@aIPSetting').contains('button[data-cy="q-btn"]', 'Add').click()
+            waitPageProgress()
+            cy.get('@aIPSetting').find(ipButtons.ipv4button + '1"]').should('contain.text', ipButtons.ipv4)
+            cy.get('@aIPSetting').contains('button', 'Remove all').click()
+            waitPageProgress()
+            cy.get('@aIPSetting').find('[data-cy^="q-chip"]').should('not.exist')
+        })
+
+        it('Delete domain', () => {
+            cy.login(ngcpConfig.username, ngcpConfig.password)
+            cy.navigateMainMenu('settings / domain-list')
+
+            cy.locationShouldBe('#/domain')
+            deleteItemOnListPageByName(domainName)
         })
     })
 })
