6daa4905cf32e265e16e572d4421b53801b00a2c
Nico Schedel <nschedel@sipwise.com>
2022-05-18 13:10:32 +0200

    TT#177856 E2ET: AUI - Customers - As Admin with role reseller I want to manage Customers
    Change-Id: I9c7d880c873cbd45d0d23649e6c4f1693caba4c6

--- cypress/integration/ngcp-admin-ui/customers.spec.js
+++ cypress/integration/ngcp-admin-ui/customers.spec.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 
 import {
+    apiCreateAdmin,
     apiCreateBillingProfile,
     apiCreateContract,
     apiCreateCustomer,
@@ -8,6 +9,7 @@ import {
     apiCreateReseller,
     apiCreateSystemContact,
     apiLoginAsSuperuser,
+    apiRemoveAdminBy,
     apiRemoveBillingProfileBy,
     apiRemoveContractBy,
     apiRemoveCustomerBy,
@@ -24,6 +26,18 @@ import {
     searchInDataTable,
     clickDataTableSelectedMoreMenuItem
 } from '../../support/ngcp-admin-ui/utils/common'
+
+const mainResellerAdmin = {
+    login: 'admin' + getRandomNum(),
+    password: 'rand0mpassword12345',
+    role: 'reseller',
+    is_master: true,
+    is_active: true,
+    show_passwords: true,
+    call_data: true,
+    billing_data: true,
+    reseller_id: null
+}
 
 const customer = {
     billing_profile_definition: 'id',
@@ -77,6 +91,7 @@ context('Customer tests', () => {
                     apiCreateContract({ data: { ...contract, contact_id: id }, authHeader }).then(({ id }) => {
                         apiCreateReseller({ data: { ...reseller, contract_id: id }, authHeader }).then(({ id }) => {
                             customerContact.reseller_id = id
+                            apiCreateAdmin({ data: { ...mainResellerAdmin, reseller_id: id }, authHeader })
                             apiCreateBillingProfile({ data: { ...billingProfile, reseller_id: id }, authHeader }).then(({ id }) => {
                                 customer.billing_profile_id = id
                             })
@@ -100,6 +115,7 @@ context('Customer tests', () => {
         after(() => {
             cy.log('Data clean up...')
             apiLoginAsSuperuser().then(authHeader => {
+                apiRemoveAdminBy({ name: mainResellerAdmin.login, authHeader })
                 apiRemoveResellerBy({ name: reseller.name, authHeader })
                 apiRemoveBillingProfileBy({ name: billingProfile.name, authHeader })
                 apiRemoveContractBy({ name: contract.external_id, authHeader })
@@ -109,86 +125,82 @@ context('Customer tests', () => {
 
         afterEach(() => {
             apiLoginAsSuperuser().then(authHeader => {
-                cy.log(customer.customer_id)
                 apiRemoveCustomerBy({ name: customer.external_id, authHeader })
-                apiRemoveCustomerContactBy({ name: customerContact.email, authHeader })
+                apiRemoveCustomerContactBy({ name: customer.email, authHeader })
             })
         })
 
-        it('Check if customer with invalid values gets rejected', () => {
-            cy.login(ngcpConfig.username, ngcpConfig.password)
-            cy.navigateMainMenu('settings / customer-list')
+        ;[
+            {
+                loginType: 'admin',
+                login: ngcpConfig.username,
+                password: ngcpConfig.password
+            },
+            {
+                loginType: 'reseller',
+                login: mainResellerAdmin.login,
+                password: mainResellerAdmin.password
+            }
+        ].forEach(({ loginType, login, password }) => {
+            context(`Admin login type: ${loginType}`, () => {
+                it('Check if customer with invalid values gets rejected', () => {
+                    cy.login(login, password)
+                    cy.navigateMainMenu('settings / customer-list')
 
-            cy.locationShouldBe('#/customer')
-            clickToolbarActionButton('customer-creation')
+                    cy.locationShouldBe('#/customer')
+                    clickToolbarActionButton('customer-creation')
 
-            cy.locationShouldBe('#/customer/create')
-            cy.get('[data-cy="aui-save-button"]').click()
-            cy.get('label[data-cy="aui-select-billing-profile"][error="true"]').should('be.visible')
-            cy.get('label[data-cy="aui-select-contact"][error="true"]').should('be.visible')
-        })
+                    cy.locationShouldBe('#/customer/create')
+                    cy.get('[data-cy="aui-save-button"]').click()
+                    cy.get('label[data-cy="aui-select-billing-profile"][error="true"]').should('be.visible')
+                    cy.get('label[data-cy="aui-select-contact"][error="true"]').should('be.visible')
+                })
 
-        it('Create a customer', () => {
-            apiLoginAsSuperuser().then(authHeader => {
-                apiRemoveCustomerBy({ name: customer.external_id, authHeader })
-                customer.external_id = 'customer' + getRandomNum()
+                it('Create a customer', () => {
+                    apiLoginAsSuperuser().then(authHeader => {
+                        apiRemoveCustomerBy({ name: customer.external_id, authHeader })
+                        customer.external_id = 'customer' + getRandomNum()
+                    })
+                    cy.login(login, password)
+                    cy.navigateMainMenu('settings / customer-list')
+
+                    cy.locationShouldBe('#/customer')
+                    clickToolbarActionButton('customer-creation')
+
+                    cy.locationShouldBe('#/customer/create')
+                    cy.auiSelectLazySelect({ dataCy: 'aui-select-contact', filter: customerContact.email, itemContains: 'contact' })
+                    cy.get('[data-cy="customer-external-id"] input').type(customer.external_id)
+                    cy.auiSelectLazySelect({ dataCy: 'aui-select-billing-profile', filter: billingProfile.name, itemContains: 'profile' })
+                    cy.get('[data-cy="aui-save-button"]').click()
+                    cy.get('div[role="alert"]').should('have.class', 'bg-positive')
+                })
+
+                it('Edit customer status to "locked"', () => {
+                    cy.login(login, password)
+                    cy.navigateMainMenu('settings / customer-list')
+
+                    cy.locationShouldBe('#/customer')
+                    searchInDataTable(customer.external_id, 'External #')
+                    cy.get('[data-cy=aui-data-table] .q-checkbox').click()
+                    clickDataTableSelectedMoreMenuItem('customer-edit')
+                    waitPageProgress()
+
+                    cy.qSelect({ dataCy: 'customer-status', filter: '', itemContains: 'Locked' })
+                    cy.get('[data-cy="aui-save-button"]').click()
+                    waitPageProgress()
+                    cy.get('[data-cy="aui-close-button"]').click()
+                    waitPageProgress()
+                    cy.get('[data-cy="q-td--status"]').contains('Locked')
+                })
+
+                it('Delete customer and check if they are deleted', () => {
+                    cy.login(login, password)
+                    cy.navigateMainMenu('settings / customer-list')
+
+                    cy.locationShouldBe('#/customer')
+                    deleteItemOnListPageBy(customer.external_id, 'External #')
+                })
             })
-            cy.login(ngcpConfig.username, ngcpConfig.password)
-            cy.navigateMainMenu('settings / customer-list')
-
-            cy.locationShouldBe('#/customer')
-            clickToolbarActionButton('customer-creation')
-
-            cy.locationShouldBe('#/customer/create')
-            cy.auiSelectLazySelect({ dataCy: 'aui-select-contact', filter: 'default', itemContains: 'default' })
-            cy.get('[data-cy="customer-external-id"] input').type(customer.external_id)
-            cy.auiSelectLazySelect({ dataCy: 'aui-select-billing-profile', filter: 'default', itemContains: 'default' })
-            cy.get('[data-cy="aui-save-button"]').click()
-            cy.get('div[role="alert"]').should('have.class', 'bg-positive')
-        })
-
-        it('Edit customer status to "locked"', () => {
-            cy.login(ngcpConfig.username, ngcpConfig.password)
-            cy.navigateMainMenu('settings / customer-list')
-
-            cy.locationShouldBe('#/customer')
-            searchInDataTable(customer.external_id, 'External #')
-            cy.get('[data-cy=aui-data-table] .q-checkbox').click()
-            clickDataTableSelectedMoreMenuItem('customer-edit')
-            waitPageProgress()
-
-            cy.qSelect({ dataCy: 'customer-status', filter: '', itemContains: 'Locked' })
-            cy.get('[data-cy="aui-save-button"]').click()
-            waitPageProgress()
-            cy.get('[data-cy="aui-close-button"]').click()
-            waitPageProgress()
-            cy.get('[data-cy="q-td--status"]').contains('Locked')
-        })
-
-        it.skip('Add and remove max. subscribers from customer', () => { // Temporarily disabled due to bug: clearing field by keyboard causes error 500
-            cy.login(ngcpConfig.username, ngcpConfig.password)
-            cy.navigateMainMenu('settings / customer-list')
-
-            cy.locationShouldBe('#/customer')
-            searchInDataTable(customer.external_id, 'External #')
-            cy.get('[data-cy="q-td--max-subscribers"] [data-cy^="aui-data-table-inline-edit--"]').click()
-            cy.get('[data-cy="aui-data-table-edit-input--popup"] input').type('50')
-            cy.contains('.q-popup-edit__buttons button', 'Save').click()
-            waitPageProgress()
-            cy.contains('[data-cy="q-td--max-subscribers"] span', '50').should('exist')
-            cy.contains('[data-cy="q-td--max-subscribers"] span', '50').click()
-            cy.get('[data-cy="aui-data-table-edit-input--popup"] input').clear() // TODO: we need to clarify why we receive error 500 from backend for empty string value
-            cy.contains('.q-popup-edit__buttons button', 'Save').click()
-            waitPageProgress()
-            cy.contains('[data-cy="q-td--max-subscribers"] button', 'add').should('exist')
-        })
-
-        it('Delete customer and check if they are deleted', () => {
-            cy.login(ngcpConfig.username, ngcpConfig.password)
-            cy.navigateMainMenu('settings / customer-list')
-
-            cy.locationShouldBe('#/customer')
-            deleteItemOnListPageBy(customer.external_id, 'External #')
         })
     })
 })
