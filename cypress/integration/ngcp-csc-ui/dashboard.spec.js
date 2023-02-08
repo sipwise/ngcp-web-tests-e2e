17e212485bfe4180ddd0523e3ec59e5650704833
Nico Schedel <nschedel@sipwise.com>
2023-02-08 13:29:36 +0100

    MT#55492 fix conversation tab test
    Change-Id: I048c5ff403b2fc0ac3d1b8a4c1040e5ce4996aef

--- cypress/integration/ngcp-csc-ui/dashboard.spec.js
+++ cypress/integration/ngcp-csc-ui/dashboard.spec.js
@@ -132,7 +132,7 @@ context('Dashboard page tests', () => {
             cy.get('div[class="q-date__calendar-item q-date__calendar-item--in"] span').contains('1').click({ force: true })
             cy.wait(1000)
             cy.get('label[data-cy="filter-to"]').click()
-            cy.get('div[class="q-date__calendar-item q-date__calendar-item--in"] span').contains(dayjs().format('DD')).click({ force: true })
+            cy.get('div[class="q-date__calendar-item q-date__calendar-item--in"] span').contains(dayjs().format('D')).click({ force: true })
 
             cy.get('div[data-cy="conversations-empty"]').should('contain.text', 'No Calls, Voicemails or Faxes found')
         })
