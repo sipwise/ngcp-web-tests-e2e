b14d4c465b716437f9292206daa672bab477ae9b
Nico Schedel <nschedel@sipwise.com>
2023-02-21 09:08:28 +0100

    MT#55492 add music_on_hold test
    Change-Id: I3d88246d6d8dd03189daec2d3f9f9c58340b05e7

---
+++ cypress/integration/ngcp-csc-ui/callsettings_general.spec.js
@@ -0,0 +1,96 @@
+/// <reference types="cypress" />
+
+import {
+    getRandomNum
+} from '../../support/ngcp-csc-ui/utils/common'
+
+import {
+    apiLoginAsSuperuser,
+    apiCreateCustomer,
+    apiCreateDomain,
+    apiCreateSubscriber,
+    apiRemoveDomainBy,
+    apiRemoveCustomerBy,
+    apiRemoveSubscriberBy
+} from '../../support/ngcp-csc-ui/utils/api'
+
+const ngcpConfig = Cypress.config('ngcpConfig')
+
+const customer = {
+    billing_profile_definition: 'id',
+    billing_profile_id: 1,
+    external_id: 'customer' + getRandomNum(),
+    contact_id: 1,
+    status: 'active',
+    type: 'sipaccount'
+}
+
+const domain = {
+    domain: 'domain' + getRandomNum(),
+    reseller_id: 1
+}
+
+const subscriber = {
+    username: 'subscriber' + getRandomNum(),
+    webusername: 'subscriber' + getRandomNum(),
+    email: 'email' + getRandomNum() + '@test.com',
+    external_id: 'subid' + getRandomNum(),
+    password: 'sub' + getRandomNum() + 'pass',
+    webpassword: 'sub' + getRandomNum() + 'pass',
+    domain: domain.domain,
+    customer_id: 0,
+    subscriber_id: 0
+}
+
+const loginInfo = {
+    username: subscriber.webusername + '@' + subscriber.domain,
+    password: subscriber.webpassword
+}
+
+context('Call Settings "General" page tests', () => {
+    context('UI Call Settings "General" tests', () => {
+        before(() => {
+            Cypress.log({ displayName: 'API URL', message: ngcpConfig.apiHost })
+            apiLoginAsSuperuser().then(authHeader => {
+                apiCreateDomain({ data: domain, authHeader })
+                apiCreateCustomer({ data: customer, authHeader }).then(({ id }) => {
+                    subscriber.customer_id = id
+                })
+            })
+        })
+
+        beforeEach(() => {
+            apiLoginAsSuperuser().then(authHeader => {
+                apiCreateSubscriber({ data: subscriber, authHeader })
+            })
+            cy.visit('/')
+        })
+
+        after(() => {
+            cy.log('Data clean up...')
+            apiLoginAsSuperuser().then(authHeader => {
+                apiRemoveDomainBy({ name: domain.domain, authHeader })
+                apiRemoveCustomerBy({ name: customer.external_id, authHeader })
+            })
+        })
+
+        afterEach(() => {
+            apiLoginAsSuperuser().then(authHeader => {
+                apiRemoveSubscriberBy({ name: subscriber.username, authHeader })
+            })
+        })
+        it('Enable "Music on Hold"', () => {
+            cy.loginUI(loginInfo.username, loginInfo.password)
+            cy.get('a[href="#/user/dashboard"]').should('be.visible')
+
+            cy.get('div[data-cy="q-item-label"]').contains('Call Settings').click()
+            cy.get('a[href="#/user/call-settings"]').click()
+
+            cy.get('div[data-cy="music-on-hold"][aria-disabled="true"]').should('not.exist')
+            cy.get('div[data-cy="music-on-hold"]').click()
+            cy.get('div[data-cy="music-on-hold"][aria-checked="true"]').should('be.visible')
+            cy.get('div[data-cy="music-on-hold"]').click()
+            cy.get('div[data-cy="music-on-hold"][aria-checked="false"]').should('be.visible')
+        })
+    })
+})
