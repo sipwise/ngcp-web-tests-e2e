ba030e50ea54e6eb77d423dac06c664b3a37302d
Nico Schedel <nschedel@sipwise.com>
2024-02-08 12:25:29 +0100

    MT#58247 disable phonebook tests for CE enviroments
    phonebooks are not supported in CE, disabling them if CE is detected
    
    Change-Id: I9b5148c6c0172147a00b79b7c6715440765fe3c1

--- cypress/e2e/ngcp-admin-ui/phonebook.cy.js
+++ cypress/e2e/ngcp-admin-ui/phonebook.cy.js
@@ -19,6 +19,8 @@ const downloadsFolder = Cypress.config('downloadsFolder')
 const fixturesFolder = Cypress.config('fixturesFolder')
 const ngcpConfig = Cypress.config('ngcpConfig')
 const path = require('path')
+var issppro = null
+
 
 const contract = {
     contact_id: 3,
@@ -50,26 +52,45 @@ context('Phonebook tests', () => {
     context('UI phonebook tests', () => {
         before(() => {
             Cypress.log({ displayName: 'API URL', message: ngcpConfig.apiHost })
-            apiLoginAsSuperuser().then(authHeader => {
-                apiCreateSystemContact({ data: systemContactDependency, authHeader }).then(({ id }) => {
-                    apiCreateContract({ data: { ...contract, contact_id: id }, authHeader }).then(({ id }) => {
-                        apiCreateReseller({ data: { ...reseller, contract_id: id }, authHeader }).then(({ id }) => {
-                            ResellerPhonebook.reseller_id = id
+            cy.intercept('GET', '**/api/platforminfo').as('platforminfo')
+            cy.login(ngcpConfig.username, ngcpConfig.password)
+            cy.wait('@platforminfo').then(({ response }) => {
+                if (response.body.type === 'sppro') {
+                    issppro = true
+                    apiLoginAsSuperuser().then(authHeader => {
+                        apiCreateSystemContact({ data: systemContactDependency, authHeader }).then(({ id }) => {
+                            apiCreateContract({ data: { ...contract, contact_id: id }, authHeader }).then(({ id }) => {
+                                apiCreateReseller({ data: { ...reseller, contract_id: id }, authHeader }).then(({ id }) => {
+                                    ResellerPhonebook.reseller_id = id
+                                })
+                            })
                         })
                     })
-                })
+                } else {
+                    issppro = false
+                    cy.log('Not a SPPRO instance, exiting test...')
+                }
             })
         })
 
         beforeEach(() => {
-            apiLoginAsSuperuser().then(authHeader => {
-                apiCreateResellerPhonebook({data: ResellerPhonebook, authHeader})
-            })
+            if (issppro) {
+                apiLoginAsSuperuser().then(authHeader => {
+                    apiCreateResellerPhonebook({data: ResellerPhonebook, authHeader})
+                })
+            } else {
+                cy.log('Not a SPPRO instance, skipping preperation...')
+            }
+
         })
 
         after(() => {
             cy.log('Data clean up...')
-            deleteDownloadsFolder()
+            if (issppro) {
+                deleteDownloadsFolder()
+            } else {
+                cy.log('Not a SPPRO instance, skipping download folder cleanup...')
+            }
             apiLoginAsSuperuser().then(authHeader => {
                 apiRemoveResellerBy({ name: reseller.name, authHeader })
                 apiRemoveContractBy({ name: contract.external_id, authHeader })
@@ -78,77 +99,106 @@ context('Phonebook tests', () => {
         })
 
         afterEach(() => {
-            apiLoginAsSuperuser().then(authHeader => {
-                apiRemoveResellerPhonebookBy({name: ResellerPhonebook.name, authHeader})
-            })
+            if (issppro) {
+                apiLoginAsSuperuser().then(authHeader => {
+                    apiRemoveResellerPhonebookBy({name: ResellerPhonebook.name, authHeader})
+                })                
+            } else {
+                cy.log('Not a SPPRO instance, skipping cleanup...')
+            }
         })
 
         it('Check if phonebook with invalid values gets rejected', () => {
-            cy.login(ngcpConfig.username, ngcpConfig.password)
-            cy.navigateMainMenu('settings / phonebook')
-            waitPageProgress()
-            cy.locationShouldBe('#/phonebook')
-            cy.get('a[data-cy="aui-list-action--add"]').click()
-            cy.get('[data-cy=aui-save-button]').click()
-            cy.get('input[data-cy="aui-select-reseller"]').parents('label').find('div[role="alert"]').contains('Input is required').should('be.visible')
-            cy.get('label[data-cy="phonebook-name"]').find('div[role="alert"]').contains('Input is required').should('be.visible')
-            cy.get('label[data-cy="phonebook-number"]').find('div[role="alert"]').contains('Input is required').should('be.visible')
+            if (issppro) {
+                cy.login(ngcpConfig.username, ngcpConfig.password)
+                cy.navigateMainMenu('settings / phonebook')
+                waitPageProgress()
+                cy.locationShouldBe('#/phonebook')
+                cy.get('a[data-cy="aui-list-action--add"]').click()
+                cy.get('[data-cy=aui-save-button]').click()
+                cy.get('input[data-cy="aui-select-reseller"]').parents('label').find('div[role="alert"]').contains('Input is required').should('be.visible')
+                cy.get('label[data-cy="phonebook-name"]').find('div[role="alert"]').contains('Input is required').should('be.visible')
+                cy.get('label[data-cy="phonebook-number"]').find('div[role="alert"]').contains('Input is required').should('be.visible')
+            } else {
+                cy.log('Not a SPPRO instance, skipping test...')
+            }
+
         })
 
         it('Create a phonebook', () => {
-            apiLoginAsSuperuser().then(authHeader => {
-                apiRemoveResellerPhonebookBy({name: ResellerPhonebook.name, authHeader})
-            })
-            cy.login(ngcpConfig.username, ngcpConfig.password)
-            cy.navigateMainMenu('settings / phonebook')
-            waitPageProgress()
-            cy.locationShouldBe('#/phonebook')
-            cy.get('a[data-cy="aui-list-action--add"]').click()
-            cy.auiSelectLazySelect({ dataCy: 'aui-select-reseller', filter: reseller.name, itemContains: reseller.name })
-            cy.get('input[data-cy="phonebook-name"]').type(ResellerPhonebook.name)
-            cy.get('input[data-cy="phonebook-number"]').type(ResellerPhonebook.number)
-            cy.get('[data-cy=aui-save-button]').click()
-            cy.get('div[role="alert"]').should('have.class', 'bg-positive')
+            if (issppro) {
+                apiLoginAsSuperuser().then(authHeader => {
+                    apiRemoveResellerPhonebookBy({name: ResellerPhonebook.name, authHeader})
+                })
+                cy.login(ngcpConfig.username, ngcpConfig.password)
+                cy.navigateMainMenu('settings / phonebook')
+                waitPageProgress()
+                cy.locationShouldBe('#/phonebook')
+                cy.get('a[data-cy="aui-list-action--add"]').click()
+                cy.auiSelectLazySelect({ dataCy: 'aui-select-reseller', filter: reseller.name, itemContains: reseller.name })
+                cy.get('input[data-cy="phonebook-name"]').type(ResellerPhonebook.name)
+                cy.get('input[data-cy="phonebook-number"]').type(ResellerPhonebook.number)
+                cy.get('[data-cy=aui-save-button]').click()
+                cy.get('div[role="alert"]').should('have.class', 'bg-positive')
+            } else {
+                cy.log('Not a SPPRO instance, skipping test...')
+            }
+
         })
 
         it('Download a phonebook CSV', () => {
-            cy.login(ngcpConfig.username, ngcpConfig.password)
-            cy.navigateMainMenu('settings / phonebook')
-            waitPageProgress()
-            cy.locationShouldBe('#/phonebook')
-            cy.get('button[data-cy="phonebook-download-csv"]').click()
-            const filename = path.join(downloadsFolder, 'reseller_phonebook_entries.csv')
-            cy.readFile(filename, 'binary', { timeout: 1000 })
-                .should(buffer => expect(buffer.length).to.be.gt(20))
+            if (issppro) {
+                cy.login(ngcpConfig.username, ngcpConfig.password)
+                cy.navigateMainMenu('settings / phonebook')
+                waitPageProgress()
+                cy.locationShouldBe('#/phonebook')
+                cy.get('button[data-cy="phonebook-download-csv"]').click()
+                const filename = path.join(downloadsFolder, 'reseller_phonebook_entries.csv')
+                cy.readFile(filename, 'binary', { timeout: 2000 })
+                    .should(buffer => expect(buffer.length).to.be.gt(20))
+            } else {
+                cy.log('Not a SPPRO instance, skipping test...')
+            }
+
         })
 
         it('Upload a phonebook CSV', () => {
-            cy.login(ngcpConfig.username, ngcpConfig.password)
-            cy.navigateMainMenu('settings / phonebook')
-            waitPageProgress()
-            cy.locationShouldBe('#/phonebook')
-            cy.get('a[data-cy="phonebook-upload-csv"]').click()
-            cy.get('input[type="file"][data-cy="phonebook-upload-field"]').selectFile(path.join(fixturesFolder, 'reseller_phonebook_entries.csv'), { force: 'true' })
-            cy.get('div[data-cy="phonebook-purge"]').click()
-            cy.get('button[data-cy="aui-save-button"]').click()
-            waitPageProgress()
-            apiLoginAsSuperuser().then(authHeader => {
-                apiRemoveResellerPhonebookBy({name: 'phonebooktestr', authHeader})
-            })
-            cy.get('div[role="alert"]').should('have.class', 'bg-positive')
-            cy.get('td[data-cy="q-td--name"] span').contains('phonebooktestr').should('be.visible')
-            cy.get('td[data-cy="q-td--number"] span').contains('12345').should('be.visible')
+            if (issppro) {
+                cy.login(ngcpConfig.username, ngcpConfig.password)
+                cy.navigateMainMenu('settings / phonebook')
+                waitPageProgress()
+                cy.locationShouldBe('#/phonebook')
+                cy.get('a[data-cy="phonebook-upload-csv"]').click()
+                cy.get('input[type="file"][data-cy="phonebook-upload-field"]').selectFile(path.join(fixturesFolder, 'reseller_phonebook_entries.csv'), { force: 'true' })
+                cy.get('div[data-cy="phonebook-purge"]').click()
+                cy.get('button[data-cy="aui-save-button"]').click()
+                waitPageProgress()
+                apiLoginAsSuperuser().then(authHeader => {
+                    apiRemoveResellerPhonebookBy({name: 'phonebooktestr', authHeader})
+                })
+                cy.get('div[role="alert"]').should('have.class', 'bg-positive')
+                cy.get('td[data-cy="q-td--name"] span').contains('phonebooktestr').should('be.visible')
+                cy.get('td[data-cy="q-td--number"] span').contains('12345').should('be.visible')
+            } else {
+                cy.log('Not a SPPRO instance, skipping test...')
+            }
+
         })
 
         it('Delete phonebook', () => {
-            cy.login(ngcpConfig.username, ngcpConfig.password)
-            cy.navigateMainMenu('settings / phonebook')
-            waitPageProgress()
-            cy.locationShouldBe('#/phonebook')
-            cy.get('span[data-cy="aui-data-table-highlighted-text"]').contains(ResellerPhonebook.name).parents('tr').find('td[data-cy="q-td--more-menu-left"]').click()
-            cy.get('div[data-cy="aui-data-table-row-menu--delete"]').click()
-            cy.get('button[data-cy="btn-confirm"]').click()
-            cy.contains('.q-table__bottom--nodata', 'No data available').should('be.visible')
+            if (issppro) {
+                cy.login(ngcpConfig.username, ngcpConfig.password)
+                cy.navigateMainMenu('settings / phonebook')
+                waitPageProgress()
+                cy.locationShouldBe('#/phonebook')
+                cy.get('span[data-cy="aui-data-table-highlighted-text"]').contains(ResellerPhonebook.name).parents('tr').find('td[data-cy="q-td--more-menu-left"]').click()
+                cy.get('div[data-cy="aui-data-table-row-menu--delete"]').click()
+                cy.get('button[data-cy="btn-confirm"]').click()
+                cy.contains('.q-table__bottom--nodata', 'No data available').should('be.visible')
+            } else {
+                cy.log('Not a SPPRO instance, skipping test...')
+            }
+
         })
     })
 })
