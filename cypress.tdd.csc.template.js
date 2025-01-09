0b0b61aa6a56b0997697c356ec5f0511eb7f3fb1
Nico Schedel <nschedel@sipwise.com>
2025-01-09 13:56:29 +0100

    MT#57568 fix csc ci and tdd templates, remove unused json config
    Change-Id: Ifdd22edefb0058e79125c2fee72c298fb45f0699
    NGCP-Flow: mr11.5
    (cherry picked from commit 3272ac2cea04b643a83783463205df7c4ad1c5e3)

--- cypress.tdd.csc.template.js
+++ cypress.tdd.csc.template.js
@@ -17,7 +17,7 @@ module.exports = defineConfig({
         },
         experimentalRunAllSpecs: true,
         baseUrl: 'https://pro-trunk.mgm.sipwise.com',
-        specPattern: 'cypress/e2e/ngcp-admin-ui/**/*.cy.{js,jsx,ts,tsx}',
-        supportFile: 'cypress/support/ngcp-admin-ui'
+        specPattern: 'cypress/e2e/ngcp-csc-ui/**/*.cy.{js,jsx,ts,tsx}',
+        supportFile: 'cypress/support/ngcp-csc-ui'
     }
 })
