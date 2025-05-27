d6bd9140300a250ef4d1d18a786590ddab0289e2
Debora Crescenzo <dcrescenzo@sipwise.com>
2025-05-27 08:52:07 +0100

    MT#62720 Fix subscriber_details test
    Commit 42c5039acec666705637d4bb48088ab347ea4fac
    accidentally reverted some of the changes
    of commit efd9cd5c319c9e8575d8b9e12c01f469d07967e8.
    We add them back with this commit.
    
    NGCP-Flow: mr11.5
    Change-Id: I26e04ac3699ddf1f8f0e8689c9f394e3c4decd52
    (cherry picked from commit 9d59e945f41ea2a8771f19fe2bda5720c0023321)

--- cypress/support/ngcp-admin-ui/e2e.js
+++ cypress/support/ngcp-admin-ui/e2e.js
@@ -879,7 +879,7 @@ export const apiRemoveEmergencyMappingContainerBy = ({ name, authHeader }) => {
         ...authHeader
     }).then(({ body }) => {
         const emcId = body?._embedded?.['ngcp:emergencymappingcontainers']?.[0]?.id
-        if (body?.total_count === 1 && emcId >= 1) {
+        if (emcId) {
             return cy.request({
                 method: 'DELETE',
                 url: `${ngcpConfig.apiHost}/api/emergencymappingcontainers/${emcId}`,
@@ -1630,7 +1630,7 @@ export const testPreferencesTextField = (name, value = 'test', onlyNumbers = fal
     cy.get('button[data-cy="preference-save"]').click()
     cy.get('@' + cyAliasName).find('label[aria-disabled="true"]').should('not.exist')
     cy.get('@' + cyAliasName).find('input').should('have.value', value)
-    cy.get('@' + cyAliasName).contains('button[data-cy="q-icon"]', 'cancel').click()
+     cy.get('@' + cyAliasName).contains('i[data-cy="q-icon"]', 'cancel').click()
     cy.get('button[data-cy="preference-save"]').click()
     cy.get('@' + cyAliasName).find('label[aria-disabled="true"]').should('not.exist')
     cy.get('@' + cyAliasName).find('input').should('have.value', '')
