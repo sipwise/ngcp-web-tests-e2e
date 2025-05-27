sipwise/ngcp-web-tests-e2e

commit 42c5039acec666705637d4bb48088ab347ea4fac

MT#62720 Upgrade Cypress in mr11.5X
NGCP-Flow: mr11.5
Change-Id: I41b19b4ce85827dd005fc1ef22f835891740b4c5

cypress/support/ngcp-admin-ui/e2e.js
@@ -879,7 +879,7 @@ export const apiRemoveEmergencyMappingContainerBy = ({ name, authHeader }) => {
         ...authHeader
     }).then(({ body }) => {
         const emcId = body?._embedded?.['ngcp:emergencymappingcontainers']?.[0]?.id
-        if (body?.total_count === 1 && emcId > 1) {
+        if (body?.total_count === 1 && emcId >= 1) {
             return cy.request({
                 method: 'DELETE',
                 url: `${ngcpConfig.apiHost}/api/emergencymappingcontainers/${emcId}`,
@@ -1556,7 +1556,7 @@ export const getRandomNum = (maxLength = 5) => Math.floor((Math.random() * Math.
 
 export const waitPageProgress = () => {
     cy.get('div[class="q-linear-progress"][role="progressbar"]').should('be.visible')
-    cy.get('div[class="q-linear-progress"][role="progressbar"]').should('not.exist')
+    cy.get('div[class="q-linear-progress"][role="progressbar"]', {timeout: 10000}).should('not.exist')
 }
 
 export const searchInDataTable = (searchText, searchCriteria = null) => {
@@ -1630,7 +1630,7 @@ export const testPreferencesTextField = (name, value = 'test', onlyNumbers = fal
     cy.get('button[data-cy="preference-save"]').click()
     cy.get('@' + cyAliasName).find('label[aria-disabled="true"]').should('not.exist')
     cy.get('@' + cyAliasName).find('input').should('have.value', value)
-    cy.get('@' + cyAliasName).contains('i[data-cy="q-icon"]', 'cancel').click()
+    cy.get('@' + cyAliasName).contains('button[data-cy="q-icon"]', 'cancel').click()
     cy.get('button[data-cy="preference-save"]').click()
     cy.get('@' + cyAliasName).find('label[aria-disabled="true"]').should('not.exist')
     cy.get('@' + cyAliasName).find('input').should('have.value', '')

cypress/e2e/ngcp-admin-ui/subscriber_details.cy.js
@@ -101,6 +101,7 @@ context('Subscriber Details tests', () => {
                 cy.login(ngcpConfig.username, ngcpConfig.password)
                 cy.navigateMainMenu('settings / subscriber')
 
+                waitPageProgress()
                 cy.locationShouldBe('#/subscriber')
                 searchInDataTable(subscriber.username)
                 cy.get('div[class="aui-data-table"] .q-checkbox').click()
@@ -147,6 +148,7 @@ context('Subscriber Details tests', () => {
                 cy.login(ngcpConfig.username, ngcpConfig.password)
                 cy.navigateMainMenu('settings / subscriber')
 
+                waitPageProgress()
                 cy.locationShouldBe('#/subscriber')
                 searchInDataTable(subscriber.username)
                 cy.get('div[class="aui-data-table"] .q-checkbox').click()
@@ -182,6 +184,7 @@ context('Subscriber Details tests', () => {
                     if (response.body.type === 'sppro') {
                         cy.navigateMainMenu('settings / subscriber')
 
+                        waitPageProgress()
                         cy.locationShouldBe('#/subscriber')
                         searchInDataTable(subscriber.username)
                         cy.get('div[class="aui-data-table"] .q-checkbox').click()
@@ -205,6 +208,7 @@ context('Subscriber Details tests', () => {
                 cy.wait('@platforminfo').then(({ response }) => {
                     if (response.body.type === 'sppro') {
                         cy.navigateMainMenu('settings / subscriber')
+                        waitPageProgress()
                         cy.locationShouldBe('#/subscriber')
                         searchInDataTable(subscriber.username)
                         cy.get('div[class="aui-data-table"] .q-checkbox').click()
@@ -249,12 +253,13 @@ context('Subscriber Details tests', () => {
                 })
             })
 
-            it('Add/Delete Secret Key Renew Notify Email to Fax Features', () => {
+            it('Add/Delete Renew Notify Email to Fax Features', () => {
                 cy.intercept('GET', '**/api/platforminfo').as('platforminfo')
                 cy.login(ngcpConfig.username, ngcpConfig.password)
                 cy.wait('@platforminfo').then(({ response }) => {
                     if (response.body.type === 'sppro') {
                         cy.navigateMainMenu('settings / subscriber')
+                        waitPageProgress()
                         cy.locationShouldBe('#/subscriber')
                         searchInDataTable(subscriber.username)
                         cy.get('div[class="aui-data-table"] .q-checkbox').click()
@@ -263,7 +268,6 @@ context('Subscriber Details tests', () => {
 
                         cy.get('div[data-cy="aui-detail-page-menu"]').contains('Fax Features').click()
                         cy.get('div[data-cy="mailtofax-enable"]').click()
-                        cy.get('input[data-cy="mailtofax-input-secret-key"]').type('testkey')
                         cy.get('label[data-cy="mailtofax-secret-key-renew-interval"]').click()
                         cy.get('div[role="listbox"]').contains('Monthly').click()
                         cy.get('[data-cy="aui-save-button"]').click()
@@ -296,6 +300,8 @@ context('Subscriber Details tests', () => {
                 cy.wait('@platforminfo').then(({ response }) => {
                     if (response.body.type === 'sppro') {
                         cy.navigateMainMenu('settings / subscriber')
+                        waitPageProgress()
+
                         cy.locationShouldBe('#/subscriber')
                         searchInDataTable(subscriber.username)
                         cy.get('div[class="aui-data-table"] .q-checkbox').click()
@@ -338,6 +344,7 @@ context('Subscriber Details tests', () => {
                 })
                 cy.login(ngcpConfig.username, ngcpConfig.password)
                 cy.navigateMainMenu('settings / subscriber')
+                waitPageProgress()
 
                 cy.locationShouldBe('#/subscriber')
                 searchInDataTable(subscriber.username)
@@ -362,6 +369,7 @@ context('Subscriber Details tests', () => {
             it('Edit Location Mapping', () => {
                 cy.login(ngcpConfig.username, ngcpConfig.password)
                 cy.navigateMainMenu('settings / subscriber')
+                waitPageProgress()
 
                 cy.locationShouldBe('#/subscriber')
                 searchInDataTable(subscriber.username)
@@ -374,14 +382,14 @@ context('Subscriber Details tests', () => {
                 cy.get('div[class="aui-data-table"] .q-checkbox').click()
                 cy.get('button[data-cy="aui-list-action--edit-menu-btn"]').click()
                 cy.get('a[data-cy="aui-data-table-row-menu--subscriberLocationMappingsEdit"]').click()
-                cy.get('label[data-cy="locationmapping-location"] i').contains('cancel')
+                cy.get('label[data-cy="locationmapping-location"] button').contains('cancel').click()
                 cy.get('input[data-cy="locationmapping-location"]').type('newtestlocation')
-                cy.get('label[data-cy="locationmapping-caller_pattern"] i').contains('cancel')
+                cy.get('label[data-cy="locationmapping-caller_pattern"] button').contains('cancel').click()
                 cy.get('input[data-cy="locationmapping-caller_pattern"]').type('newtestcallerpattern')
-                cy.get('label[data-cy="locationmapping-callee_pattern"] i').contains('cancel')
+                cy.get('label[data-cy="locationmapping-callee_pattern"] button').contains('cancel').click()
                 cy.get('input[data-cy="locationmapping-callee_pattern"]').type('newtestcalleepattern')
                 cy.qSelect({ dataCy: 'locationmapping-mode', filter: 'Add', itemContains: 'Add' })
-                cy.get('label[data-cy="locationmapping-to_username"] i').contains('cancel')
+                cy.get('label[data-cy="locationmapping-to_username"] button').contains('cancel').click()
                 cy.get('input[data-cy="locationmapping-to_username"]').type('newtestusername')
                 cy.get('div[aria-label="Enabled"]').click()
                 cy.get('[data-cy="aui-save-button"]').click()
@@ -392,6 +400,7 @@ context('Subscriber Details tests', () => {
             it('Delete Location Mapping', () => {
                 cy.login(ngcpConfig.username, ngcpConfig.password)
                 cy.navigateMainMenu('settings / subscriber')
+                waitPageProgress()
 
                 cy.locationShouldBe('#/subscriber')
                 searchInDataTable(subscriber.username)

bin/run-docker.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-readonly DOCKER_GLOBAL_IMAGE_NAME='docker.mgm.sipwise.com/ngcp-web-tests-e2e-bullseye:latest'
-readonly DOCKER_LOCAL_IMAGE_NAME='ngcp-web-tests-e2e-bullseye:latest'
+readonly DOCKER_GLOBAL_IMAGE_NAME='docker.mgm.sipwise.com/ngcp-web-tests-e2e-bookworm:mr11.5'
+readonly DOCKER_LOCAL_IMAGE_NAME='ngcp-web-tests-e2e-bookworm:mr11.5'
 
 echo "Running Cypress test headless in Docker"
 

cypress.ci.aui.template.js
@@ -7,6 +7,8 @@ module.exports = defineConfig({
         password: 'administrator'
     },
     experimentalMemoryManagement: true,
+    numTestsKeptInMemory: 1,
+    retries: 1,
     screenshotOnRunFailure: true,
     video: true,
     videoUploadOnPasses: true,

cypress.ci.csc.template.js
@@ -7,9 +7,10 @@ module.exports = defineConfig({
         password: 'administrator'
     },
     experimentalMemoryManagement: true,
+    numTestsKeptInMemory: 1,
+    retries: 1,
     screenshotOnRunFailure: true,
     video: true,
-    videoUploadOnPasses: true,
     viewportHeight: 800,
     viewportWidth: 1280,
     watchForFileChanges: false,

cypress.tdd.aui.template.js
@@ -2,20 +2,26 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
     ngcpConfig: {
+        // apiHost always needs :1443 at the end
         apiHost: 'https://pro-trunk.mgm.sipwise.com:1443',
         username: 'administrator',
         password: 'administrator'
     },
+    experimentalMemoryManagement: true,
+    // Enable this if you run out of memory while running tests. You can specify the amount of tests that should be kept in RAM.
+    // Keep in mind that you have to stop tests when an error occurs to look at test details.
+    // Otherwise test details will be discarded after some time.
+    // numTestsKeptInMemory: 2,
+    retries: 1,
     viewportHeight: 800,
     viewportWidth: 1280,
     watchForFileChanges: false,
     e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
         setupNodeEvents (on, config) {
             return require('./cypress/plugins/index.js')(on, config)
         },
         experimentalRunAllSpecs: true,
+        // For AUI tests, baseUrl NEEDS to have :1443 at the end
         baseUrl: 'https://pro-trunk.mgm.sipwise.com:1443',
         specPattern: 'cypress/e2e/ngcp-admin-ui/**/*.cy.{js,jsx,ts,tsx}',
         supportFile: 'cypress/support/ngcp-admin-ui'

cypress.tdd.csc.template.js
@@ -2,20 +2,26 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
     ngcpConfig: {
+        // apiHost always needs :1443 at the end
         apiHost: 'https://pro-trunk.mgm.sipwise.com:1443',
         username: 'administrator',
         password: 'administrator'
     },
+    experimentalMemoryManagement: true,
+    // Enable this if you run out of memory while running tests. You can specify the amount of tests that should be kept in RAM.
+    // Keep in mind that you have to stop tests when an error occurs to look at test details.
+    // Otherwise test details will be discarded after some time.
+    // numTestsKeptInMemory: 2,
+    retries: 1,
     viewportHeight: 800,
     viewportWidth: 1280,
     watchForFileChanges: false,
     e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
         setupNodeEvents (on, config) {
             return require('./cypress/plugins/index.js')(on, config)
         },
         experimentalRunAllSpecs: true,
+        // For AUI tests, baseUrl NEEDS to have :1443 at the end
         baseUrl: 'https://pro-trunk.mgm.sipwise.com',
         specPattern: 'cypress/e2e/ngcp-csc-ui/**/*.cy.{js,jsx,ts,tsx}',
         supportFile: 'cypress/support/ngcp-csc-ui'

t/Dockerfile
@@ -5,29 +5,30 @@ FROM docker.mgm.sipwise.com/sipwise-bookworm:latest
 # is updated with the current date. It will force refresh of all
 # of the base images and things like `apt-get update` won't be using
 # old cached versions when the Dockerfile is built.
-ENV REFRESHED_AT 2023-07-28
+ENV REFRESHED_AT=2025-05-20
 
 RUN apt-get update && \
     apt-get install --assume-yes --no-install-recommends \
         bzip2 \
-        nodejs \
-        yarnpkg \
         libdbus-glib-1-2 libgtk2.0-0 libgtk-3-0 libnotify-dev libgconf-2-4 libgbm-dev libnss3 libxss1 libasound2 libxtst6 \
+        nodejs \
         wget \
+        yarnpkg \
         xauth \
-        xvfb && \
+        xvfb \
+        xz-utils && \
     apt-get clean
 
-# installing Chrome (114.0.5735.198)
-RUN wget https://deb.sipwise.com/files/google-chrome-stable_114_amd64.deb && \
-    apt install -y ./google-chrome-stable_114_amd64.deb  && \
-    rm -f google-chrome-stable_114_amd64.deb
+# installing Chrome (136.0.7103.59)
+RUN wget https://deb.sipwise.com/files/google-chrome-stable_136_amd64.deb && \
+    apt install -y ./google-chrome-stable_136_amd64.deb  && \
+    rm -f google-chrome-stable_current_amd64.deb
 
-# installing firefox (lang=en_US) (114.0.2)
-RUN wget http://deb.sipwise.com/files/firefox-114.0.2.tar.bz2 && \
-    tar xvf firefox-114.0.2.tar.bz2 && \
+# installing firefox (lang=en_US) (138.0.1)
+RUN wget https://deb.sipwise.com/files/firefox-138.0.1.tar.xz && \
+    tar xvf firefox-138.0.1.tar.xz && \
     ln -s /home/selenium/firefox/firefox /usr/bin/firefox && \
-    rm -f firefox-114.0.2.tar.bz2
+    rm -f firefox-138.0.1.tar.xz
 
 WORKDIR /code
 ADD package.json /code/
@@ -41,20 +42,31 @@ RUN echo "cd /code && ./run_web_e2e" >/root/.bash_history
 
 ################################################################################
 # Instructions for usage
-# ----------------------
-# When you want to build the base image from scratch
-# (jump to the next section if you don't want to build yourself!):
+# ------------------------------------------------------------------------------  
+# to run the tests, you need to follow 3 STEPS:
 #
-# NOTE: run the following command from root folder of git repository to build docker image locally and run the container:
-# % docker build --tag="ngcp-web-tests-e2e-bookworm" -f ./t/Dockerfile .
-# % docker run --rm -i -t -v "/${PWD}/cypress:/code/cypress:rw" -v "/${PWD}/package.json:/code/package.json:ro" -v "/${PWD}/cypress.template.json:/code/cypress.template.json:ro" -v "/${PWD}/t/run_web_e2e:/code/run_web_e2e:ro" ngcp-web-tests-e2e-bullseye:latest bash
+# Go to 1a if you want to build the base image from scratch or
+# Go to 1b if you want to use the existing docker image.
 #
-# Use the existing docker image:
-# % docker pull docker.mgm.sipwise.com/ngcp-web-tests-e2e-bookworm
+# 1a. Build the base image from scratch
+# % docker build --tag="ngcp-web-tests-e2e-bookworm:mr11.5" -f ./t/Dockerfile .
 #
-# NOTE: run the following command from root folder of git repository to run a new container:
-# % docker run --rm -i -t -v "/${PWD}/cypress:/code/cypress:rw" -v "/${PWD}/package.json:/code/package.json:ro" -v "/${PWD}/cypress.template.json:/code/cypress.template.json:ro" -v "/${PWD}/t/run_web_e2e:/code/run_web_e2e:ro" docker.mgm.sipwise.com/ngcp-web-tests-e2e-bullseye:latest bash
+# 1b. Use the existing docker image:
+# % docker pull docker.mgm.sipwise.com/ngcp-web-tests-e2e-bookworm:mr11.5
 #
-# Inside docker:
-#   cd /code && ./run_web_e2e <parameters>
+# 2. Run the container:
+# CSC - run the following command from root folder of git repository to run a new container:
+# % docker run --rm -i -t -v "/${PWD}/cypress:/code/cypress:rw" -v "/${PWD}/package.json:/code/package.json:ro" -v "/${PWD}/cypress.ci.csc.template.js:/code/cypress.template.js:ro" -v "/${PWD}/t/run_web_e2e:/code/run_web_e2e:ro" docker.mgm.sipwise.com/ngcp-web-tests-e2e-bookworm:mr11.5 bash
+#
+# AUI - run the following command from root folder of git repository to run a new container:
+# % docker run --rm -i -t -v "/${PWD}/cypress:/code/cypress:rw" -v "/${PWD}/package.json:/code/package.json:ro" -v "/${PWD}/cypress.ci.aui.template.js:/code/cypress.template.js:ro" -v "/${PWD}/t/run_web_e2e:/code/run_web_e2e:ro" docker.mgm.sipwise.com/ngcp-web-tests-e2e-bookworm:mr11.5 bash
+#
+# NOTE: When you are working on Mac, add the --platform linux/amd64 flag  after "docker run" and it will create an amd64 image instead of an arm64 image.
+#
+# 3. Once you are inside the docker run:
+#   ./run_web_e2e <parameters>
+# The parameters are:
+#    application_to_test="$1" ( mandatory: aui || csc)
+#    ui_address="$2" (mandatory)
+#    api_address="$3" (mandatory, eg. https://{ip}:1443)
 ################################################################################

commit 9d59e945f41ea2a8771f19fe2bda5720c0023321

MT#62720 Fix subscriber_details test
Commit 42c5039acec666705637d4bb48088ab347ea4fac
accidentally reverted some of the changes
of commit efd9cd5c319c9e8575d8b9e12c01f469d07967e8.
We add them back with this commit.

NGCP-Flow: mr11.5
Change-Id: I26e04ac3699ddf1f8f0e8689c9f394e3c4decd52

cypress/support/ngcp-admin-ui/e2e.js
@@ -879,7 +879,7 @@ export const apiRemoveEmergencyMappingContainerBy = ({ name, authHeader }) => {
         ...authHeader
     }).then(({ body }) => {
         const emcId = body?._embedded?.['ngcp:emergencymappingcontainers']?.[0]?.id
-        if (body?.total_count === 1 && emcId >= 1) {
+        if (emcId) {
             return cy.request({
                 method: 'DELETE',
                 url: `${ngcpConfig.apiHost}/api/emergencymappingcontainers/${emcId}`,
@@ -1630,7 +1630,7 @@ export const testPreferencesTextField = (name, value = 'test', onlyNumbers = fal
     cy.get('button[data-cy="preference-save"]').click()
     cy.get('@' + cyAliasName).find('label[aria-disabled="true"]').should('not.exist')
     cy.get('@' + cyAliasName).find('input').should('have.value', value)
-    cy.get('@' + cyAliasName).contains('button[data-cy="q-icon"]', 'cancel').click()
+     cy.get('@' + cyAliasName).contains('i[data-cy="q-icon"]', 'cancel').click()
     cy.get('button[data-cy="preference-save"]').click()
     cy.get('@' + cyAliasName).find('label[aria-disabled="true"]').should('not.exist')
     cy.get('@' + cyAliasName).find('input').should('have.value', '')

cypress/e2e/ngcp-admin-ui/subscriber_details.cy.js
@@ -382,14 +382,14 @@ context('Subscriber Details tests', () => {
                 cy.get('div[class="aui-data-table"] .q-checkbox').click()
                 cy.get('button[data-cy="aui-list-action--edit-menu-btn"]').click()
                 cy.get('a[data-cy="aui-data-table-row-menu--subscriberLocationMappingsEdit"]').click()
-                cy.get('label[data-cy="locationmapping-location"] button').contains('cancel').click()
+                cy.get('label[data-cy="locationmapping-location"] i').contains('cancel')
                 cy.get('input[data-cy="locationmapping-location"]').type('newtestlocation')
-                cy.get('label[data-cy="locationmapping-caller_pattern"] button').contains('cancel').click()
+                cy.get('label[data-cy="locationmapping-caller_pattern"] i').contains('cancel')
                 cy.get('input[data-cy="locationmapping-caller_pattern"]').type('newtestcallerpattern')
-                cy.get('label[data-cy="locationmapping-callee_pattern"] button').contains('cancel').click()
+                  cy.get('label[data-cy="locationmapping-callee_pattern"] i').contains('cancel')
                 cy.get('input[data-cy="locationmapping-callee_pattern"]').type('newtestcalleepattern')
                 cy.qSelect({ dataCy: 'locationmapping-mode', filter: 'Add', itemContains: 'Add' })
-                cy.get('label[data-cy="locationmapping-to_username"] button').contains('cancel').click()
+               cy.get('label[data-cy="locationmapping-to_username"] i').contains('cancel')
                 cy.get('input[data-cy="locationmapping-to_username"]').type('newtestusername')
                 cy.get('div[aria-label="Enabled"]').click()
                 cy.get('[data-cy="aui-save-button"]').click()