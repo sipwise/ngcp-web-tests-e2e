d9897190894bd1090f87a90e0b66c0469e4ff04a
Nico Schedel <nschedel@sipwise.com>
2025-12-04 14:01:19 +0100

    MT#64015 fix edit profilepackage test, remove backspace command
    Change-Id: I574c671620294605b85e8a9858dda3ce8bdc46e7
    (cherry picked from commit 2b86f178da811880acfa7bcb3922b3d87311ee96)

--- cypress/e2e/ngcp-aui/group1/profilepackages.cy.js
+++ cypress/e2e/ngcp-aui/group1/profilepackages.cy.js
@@ -166,7 +166,6 @@ context('Profile package tests', () => {
             cy.get('a[data-cy="aui-data-table-row-menu--billingProfilePackageEdit"]').click()
             cy.get('input[data-cy="profilepackages-description"]').clear().type('testDescription')
             cy.get('input[data-cy="profilepackages-balanceinterval"]').clear().type('10')
-            cy.get('input[data-cy="aui-select-initial-billing-profile"]').click().type('{backspace}')
             cy.auiSelectLazySelect({ dataCy: 'aui-select-initial-billing-profile', filter: billingProfile.name, itemContains: billingProfile.name })
             cy.get('[data-cy="aui-save-button"]').click()
             cy.get('div[role="alert"]').should('have.class', 'bg-positive')
