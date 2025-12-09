6de574e8e0e0743f56d8d4cf61436ac02193a8d6
Nico Schedel <nschedel@sipwise.com>
2025-12-09 10:30:26 +0100

    MT#63950 remove problematic waitPageProgress() calls in header manipulations
    NGCP-Flow: mr12.5
    
    Change-Id: I0f51f14aa6815e514c4fb63516a7fc3643656b67

--- cypress/e2e/ngcp-aui/group1/headermanipulations.cy.js
+++ cypress/e2e/ngcp-aui/group1/headermanipulations.cy.js
@@ -218,7 +218,7 @@ context('Header manipulation tests', () => {
                 waitPageProgressAUI()
                 cy.get('input[data-cy="header-rule-set-description"]').clear().type('testdescription')
                 cy.get('[data-cy="aui-save-button"]').click()
-                waitPageProgressAUI()
+                cy.get('div[role="alert"]').should('have.class', 'bg-positive')
                 cy.get('[data-cy="aui-close-button"]').click()
                 cy.get('td[data-cy="q-td--description"]').should('contain.text', 'testdescription')
             } else {
@@ -514,7 +514,7 @@ context('Header manipulation tests', () => {
                 waitPageProgressAUI()
                 cy.qSelect({ dataCy: 'headerruleconditions-expression', itemContains: 'contains' })
                 cy.get('[data-cy="aui-save-button"]').click()
-                waitPageProgressAUI()
+                cy.get('div[role="alert"]').should('have.class', 'bg-positive')
                 cy.get('[data-cy="aui-close-button"]').click()
                 cy.get('td[data-cy="q-td--expression"]').should('contain.text', 'contains')
             } else {
