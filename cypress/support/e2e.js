0cf22ed61ab720ca86c6d4b6e7b4218673c20e7c
Nico Schedel <nschedel@sipwise.com>
2026-09-08 12:47:16 +0200

    MT#65940 fix auiSelectLazySelect weird behaviour when calling it multiple times in a row
    make sure to wait until the listbox disappears before running a new auiSelectLazySelect instance.
    
    Change-Id: I5b71bb6e1053db3f4dbf54a61d993489e87a8c4e

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -133,6 +133,7 @@ Cypress.Commands.add('auiSelectLazySelect',
                 cy.get(dropdownListId + ' .q-linear-progress').should('not.exist')
                 cy.wait(500)
                 cy.contains(`${dropdownListId} .q-item`, itemContains).last().click()
+                cy.get('div[role="listbox"]').should('not.exist')
             })
         })
     }
